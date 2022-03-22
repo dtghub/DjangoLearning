c855ffa1ae06e7e9b9758b5e443d23e01500bfb1
Derek Todd <derekhtodd@yahoo.co.uk>
2022-03-22 20:14:31 +0000

    fix stupid type error

--- courses/views.py
+++ courses/views.py
@@ -5,6 +5,6 @@ from courses.models import Course
 def index(request):
     course_list=Course.objects.order_by('title')
     context_dict = {}
-    context_dict = course_list
+    context_dict['courses'] = course_list
     return render(request, 'courses/index.html', context=context_dict)
 
