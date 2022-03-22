c6a9de6a6662ffa7b447b84b310c11cf42dfb1b8
Derek Todd <derekhtodd@yahoo.co.uk>
2022-03-22 17:31:11 +0000

    set up models

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,20 @@
+from datetime import date
+from turtle import title
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Course_type(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+
+    def __str__(self):
+        return self.name
+    
+class CourseDetails(models.Model):
+    Course_type=models.ForeignKey(Course_type, on_delete=models.CASCADE)
+    title=models.CharField(max_length=128)
+    starting_date=models.DateField(default=date.today)
+    course_description=models.CharField(max_length=512)
+
+    def __str__(self):
+        return self.title
+    
